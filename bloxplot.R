947851bdeff9326b4f18994a92fc710474cb4be7
Jadson Moura <56553495+AruomnosdaJ@users.noreply.github.com>
2019-10-18 21:16:10 -0300

    Add files via upload

---
+++ bloxplot.R
@@ -0,0 +1,75 @@
+library(shiny)
+library(datasets)
+
+# Usando o conjunto de dados `mtcars`.
+# Rotulando a coluna am para melhor analise.
+
+mpgData <- mtcars
+mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
+mpgData$vs <- factor(mpgData$vs, labels = c("V-shaped", "straight"))
+
+# Define UI.
+ui <- fluidPage(
+  
+  titlePanel("Shiny mtcars"),
+  
+  # Sidebar layout com definições de input e output.
+  sidebarLayout(
+    
+    # Sidebar panel para inputs.
+    sidebarPanel(
+      
+      # Input: Seleciona as variáveis para plotagem
+      # em relação a mpg.
+      selectInput("variable", "Variável:",
+                  c("Carburadores" = "carb",
+                    "Cilindros" = "cyl",
+                    "Marchas" = "gear",
+                    "Motor" = "vs",
+                    "Transmissão" = "am")),
+      
+      # Input: Caixa de seleção para inclusão de outliers.
+      checkboxInput("outliers", "Show outliers", TRUE)
+      
+    ),
+    
+    # Main panel para exibir os outputs
+    mainPanel(
+      
+      # Output: Formatar texto para caption.
+      h3(textOutput("caption")),
+      
+      # Output: Plot da variável selecionada em relação a mpg.
+      plotOutput("mpgPlot")
+      
+    )
+  )
+)
+
+# Define o server para gerar os outputs.
+server <- function(input, output) {
+  
+  # Título do gráfico.
+
+  formulaText <- reactive({
+    paste("mpg ~", input$variable)
+  })
+  
+  # Retornar a formula text como caption.
+  output$caption <- renderText({
+    formulaText()
+  })
+  
+  # Gerar o gráfico da variável selecionada em relação a mpg,
+  # e exclui valores outliers se pedido.
+  output$mpgPlot <- renderPlot({
+    boxplot(as.formula(formulaText()),
+            data = mpgData,
+            outline = input$outliers,
+            col = "#40E0D0", pch = 16)
+  })
+  
+}
+
+# Criar Shiny app.
+shinyApp(ui, server)
---
+++ histogram.R
@@ -0,0 +1,43 @@
+library(shiny)
+library(datasets)
+
+# Usando o conjunto de dados `mtcars`.
+# 
+
+mpg <- mtcars
+# Define UI.
+ui <- fluidPage(
+  
+  titlePanel("Shiny mtcars"),
+  
+  sidebarLayout(
+    
+    selectInput("Ind","Variável",
+                choices = names(mpg)),
+      
+    # Main panel para exibir os outputs
+    mainPanel(
+      # Output: Plot da variável selecionada em relação a mpg.
+      plotOutput("hist")
+      
+    )
+  )
+)
+
+
+# Define o server para gerar os outputs.
+server <- function(input, output, session) {
+  data1 <- reactive({
+    input$Ind
+  })
+  # Gerar o gráfico da variável selecionada.
+  output$hist <- renderPlot({
+    req(data1())
+    hist(mpg[[data1()]], col = "#007481", border="black",
+         xlab = " ", main = " ")
+  }) 
+}
+
+# Criar Shiny app.
+shinyApp(ui, server)
+
